e987b1d30143520a68d2ec08cfa1e44f090b4f0a
Reginaldo Rampasso <49074271+knowzinho@users.noreply.github.com>
2024-03-12 21:20:13 +0000

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,7 @@
-import fire
-def hello(name="World"):
-  return "Hello %s!" % name
+# import fire
+# def hello(name="World"):
+#   return "Hello %s!" % name
 
-if __name__ == '__main__':
-  fire.Fire(hello)
+# if __name__ == '__main__':
+#   fire.Fire(hello)
+print("Hello World!")
